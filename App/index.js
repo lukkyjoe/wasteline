836da35d6c1334d07e77a80b4541ad3181fc33ab
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-05 08:20:32 -0700

    Remove irrelevant mock data.

--- App/index.js
+++ App/index.js
@@ -3,10 +3,9 @@ import App from './containers/AppContainer';
 import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import configureStore from './../redux/store';
-import mockData from './../data/data.json';
+// import mockData from './../data/data.json';
 
 let initialState = {
-  items: mockData,
   currentProfile: {},
   showItem: false, 
   upvotes: 0
